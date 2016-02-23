60861e3cbb81f8c99948b5e75ebe9b91319eabbb
wu.yuan <wu.yuan@ck-telecom.com>
2016-02-23 15:21:42 +0800

    write the viewpager frame

--- gen/com/ckt/io/R.java
+++ gen/com/ckt/io/R.java
@@ -23,12 +23,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int send=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080003;
+        public static final int button2=0x7f080004;
+        public static final int img_connect=0x7f080000;
+        public static final int textView1=0x7f080002;
+        public static final int viewPager1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_file_explorer=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
